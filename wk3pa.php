4bf2e6c62ab7e56d68fe964c9579af8e813104ef
Randall Lowe <rlowe1993@gmail.com>
2026-09-22 18:23:22 -0400

    Refactor variable names for consistency and clarity in personal information management

--- wk3pa.php
+++ wk3pa.php
@@ -7,8 +7,8 @@
     $conn = mysqli_connect($hostname, $username, $password, $dbname);
 
     //Establish variables to support add/edit/delete
-    $personNo = -1;
-    $name = "";
+    $userNo = -1;
+    $fullName = "";
     $dob = "";
     $favColor = "";
     $favPlace = "";
@@ -20,8 +20,8 @@
     $update = false;
     $delete = false;
 
-    if (isset($_POST['person_no'])) {
-        $personNo = $_POST['person_no'];
+    if (isset($_POST['user_no'])) {
+        $userNo = $_POST['user_no'];
         $add = isset($_POST['add']);
         $update = isset($_POST['update']);
         $edit = isset($_POST['edit']);
@@ -30,20 +30,20 @@
 
     if ($add) {
         //Need to add a new person
-        $name = $_POST['name'];
+        $fullName = $_POST['full_name'];
         $dob = $_POST['dob'];
         $favColor = $_POST['fav_color'];
         $favPlace = $_POST['fav_place'];
         $nickname = $_POST['nickname'];
 
         $addQuery = "INSERT INTO
-            personalinfo (Name, DOB, FavoriteColor, FavoritePlace, Nickname)
-            VALUES ('$name', '$dob', '$favColor', '$favPlace', '$nickname')";
+            personal_info (FullName, Birthdate, FavoriteColor, FavoritePlace, Nickname)
+            VALUES ('$fullName', '$dob', '$favColor', '$favPlace', '$nickname')";
         mysqli_query($conn, $addQuery);
 
         //Clear the fields
-        $personNo = -1;
-        $name = "";
+        $userNo = -1;
+        $fullName = "";
         $dob = "";
         $favColor = "";
         $favPlace = "";
@@ -51,34 +51,34 @@
     }
     else if ($edit) {
         //Get the person information
-        $selQuery = "SELECT * FROM personalinfo WHERE PersonNo = $personNo";
+        $selQuery = "SELECT * FROM personal_info WHERE UserNo = $userNo";
         $result = mysqli_query($conn, $selQuery);
         $personInfo = mysqli_fetch_assoc($result);
 
-        $name = $personInfo['Name'];
-        $dob = $personInfo['DOB'];
+        $fullName = $personInfo['FullName'];
+        $dob = $personInfo['Birthdate'];
         $favColor = $personInfo['FavoriteColor'];
         $favPlace = $personInfo['FavoritePlace'];
         $nickname = $personInfo['Nickname'];
     }
     else if ($update) {
         //Updated values submitted
-        $name = $_POST['name'];
+        $fullName = $_POST['full_name'];
         $dob = $_POST['dob'];
         $favColor = $_POST['fav_color'];
         $favPlace = $_POST['fav_place'];
         $nickname = $_POST['nickname'];
 
-        $updQuery = "UPDATE personalinfo SET
-            Name = '$name', DOB = '$dob',
+        $updQuery = "UPDATE personal_info SET
+            FullName = '$fullName', Birthdate = '$dob',
             FavoriteColor = '$favColor', FavoritePlace = '$favPlace',
             Nickname = '$nickname'
-            WHERE PersonNo = $personNo";
+            WHERE UserNo = $userNo";
         mysqli_query($conn, $updQuery);
 
         //Clear the fields
-        $personNo = -1;
-        $name = "";
+        $userNo = -1;
+        $fullName = "";
         $dob = "";
         $favColor = "";
         $favPlace = "";
@@ -86,13 +86,13 @@
     }
     else if ($delete) {
         //Need to delete the selected person
-        $delQuery = "DELETE FROM personalinfo WHERE PersonNo = $personNo";
+        $delQuery = "DELETE FROM personal_info WHERE UserNo = $userNo";
         mysqli_query($conn, $delQuery);
-        $personNo = -1;
+        $userNo = -1;
     }
 
     //Query for all people
-    $query = "SELECT * FROM personalinfo";
+    $query = "SELECT * FROM personal_info";
     $result = mysqli_query($conn, $query);
 ?>
 
@@ -141,9 +141,9 @@
 
             <?php while($row = mysqli_fetch_array($result)):;?>
             <tr>
-                <td><?php echo $row["PersonNo"];?></td>
-                <td><?php echo $row["Name"];?></td>
-                <td><?php echo $row["DOB"];?></td>
+                <td><?php echo $row["UserNo"];?></td>
+                <td><?php echo $row["FullName"];?></td>
+                <td><?php echo $row["Birthdate"];?></td>
                 <td><?php echo $row["FavoriteColor"];?></td>
                 <td><?php echo $row["FavoritePlace"];?></td>
                 <td><?php echo $row["Nickname"];?></td>
@@ -151,26 +151,26 @@
                     <form method="POST">
                         <input type="submit" value="Edit" name="edit">
                         <input type="hidden"
-                            value="<?php echo $row["PersonNo"]; ?>"
-                            name="person_no">
+                            value="<?php echo $row["UserNo"]; ?>"
+                            name="user_no">
                     </form>
                 </td>
                 <td>
                     <form method="POST">
                         <input type="submit" value="Delete" name="delete">
                         <input type="hidden"
-                            value="<?php echo $row["PersonNo"]; ?>"
-                            name="person_no">
+                            value="<?php echo $row["UserNo"]; ?>"
+                            name="user_no">
                     </form>
                 </td>
             </tr>
             <?php endwhile;?>
         </table>
         <form method="POST">
-            <input type="hidden" value="<?php echo $personNo; ?>" name="person_no">
-            <h3>Enter your name: <input type="text" name="name"
-                value="<?php echo $name; ?>"></h3>
-            <h3>Enter your date of birth: <input type="date" name="dob"
+            <input type="hidden" value="<?php echo $userNo; ?>" name="user_no">
+            <h3>Enter your name: <input type="text" name="full_name"
+                value="<?php echo $fullName; ?>"></h3>
+            <h3>Enter your date of birth: <input type="text" name="dob"
                 value="<?php echo $dob; ?>"></h3>
             <h3>Enter your favorite color: <input type="text" name="fav_color"
                 value="<?php echo $favColor; ?>"></h3>
